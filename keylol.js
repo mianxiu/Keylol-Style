032a731cf8d7fe5a073e7552f869feaf44fdf38b
mianxiu <mianxiu@users.noreply.github.com>
2020-04-06 21:56:10 +0800

    Update keylol.js

--- keylol.js
+++ keylol.js
@@ -8,35 +8,39 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
-   document.querySelectorAll(`link[rel=stylesheet]`).forEach(e=>{
-       e.remove()})
+    document.querySelectorAll(`link[rel=stylesheet]`).forEach(e => {
+        e.remove()
+    })
 
-// 移动 用户栏
-    document.querySelector('#nav-menu>ul').parentNode.insertBefore(document.querySelector('.tb-container'),null)
+    // 移动用户栏&LOGO
+    const navMenuUl = document.querySelector('#nav-menu>ul ')
+    navMenuUl.parentNode.insertBefore(document.querySelector('.tb-container'), null)
+    navMenuUl.insertBefore(document.querySelector('#nav-logo'), navMenuUl.childNodes[0])
 
-    var css =`
+
+    var css = `
 
 `
 
-if (typeof GM_addStyle != "undefined") {
-    GM_addStyle(css);
-} else if (typeof PRO_addStyle != "undefined") {
-    PRO_addStyle(css);
-} else if (typeof addStyle != "undefined") {
-    addStyle(css);
-} else {
-    var node = document.createElement("style");
-    node.type = "text/css";
-    node.appendChild(document.createTextNode(css));
-    var heads = document.getElementsByTagName("head");
-    if (heads.length > 0) {
-        heads[0].appendChild(node);
+    if (typeof GM_addStyle != "undefined") {
+        GM_addStyle(css);
+    } else if (typeof PRO_addStyle != "undefined") {
+        PRO_addStyle(css);
+    } else if (typeof addStyle != "undefined") {
+        addStyle(css);
     } else {
-        // no head yet, stick it whereever
-        document.documentElement.appendChild(node);
+        var node = document.createElement("style");
+        node.type = "text/css";
+        node.appendChild(document.createTextNode(css));
+        var heads = document.getElementsByTagName("head");
+        if (heads.length > 0) {
+            heads[0].appendChild(node);
+        } else {
+            // no head yet, stick it whereever
+            document.documentElement.appendChild(node);
+        }
     }
-}
     // Your code here...
 })();
