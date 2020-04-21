fda4a56959c2ee710b17722fb064d11338e26b9c
mianxiu <mianxiu@users.noreply.github.com>
2020-04-21 21:20:26 +0800

    backup

--- keylol.js
+++ keylol.js
@@ -18,6 +18,7 @@
             e.remove()
 
         })
+
     }
 
     var targetNode = document.documentElement
@@ -28,37 +29,37 @@
     var observer = new MutationObserver(clearCss);
     observer.observe(targetNode, observerOptions);
 
-    // 移动 用户栏&LOGO
-    const navMenuUl = document.querySelector('#nav-menu>ul ')
-    navMenuUl.parentNode.insertBefore(document.querySelector('.tb-container'), null)
-    navMenuUl.insertBefore(document.querySelector('#nav-logo'), navMenuUl.childNodes[0])
+
+
+    // 移动节点
+    var moveElement = function () {
+
+        // 用户栏&LOGO
+        let navMenuUl = document.querySelector('#nav-menu>ul ')
+        navMenuUl.parentNode.insertBefore(document.querySelector('.tb-container'), null)
+        navMenuUl.insertBefore(document.querySelector('#nav-logo'), navMenuUl.childNodes[0])
+
+    }
+
+    window.onload = moveElement
 
 
     var css = `
+body{
+background:green;
+}
 
 `
-
-
-
-
     // 添加css
-    if (typeof GM_addStyle != "undefined") {
-        GM_addStyle(css);
-    } else if (typeof PRO_addStyle != "undefined") {
-        PRO_addStyle(css);
-    } else if (typeof addStyle != "undefined") {
-        addStyle(css);
-    } else {
-        var node = document.createElement("style");
-        node.type = "text/css";
-        node.appendChild(document.createTextNode(css));
-        var heads = document.getElementsByTagName("head");
-        if (heads.length > 0) {
-            heads[0].appendChild(node);
-        } else {
-            // no head yet, stick it whereever
-            document.documentElement.appendChild(node);
-        }
-    }
+    var node = document.createElement("style");
+    node.type = "text/css";
+    node.appendChild(document.createTextNode(css));
+    var html = document.querySelector("html");
+    document.documentElement.appendChild(node);
+
+
+
     // Your code here...
+    // 取消observer
+    setTimeout(() => { observer.disconnect() }, 10000)
 })();
