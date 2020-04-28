bff26e6952e81b7ef4aa6a3b26caf224196fdaec
mianxiu <mianxiu@users.noreply.github.com>
2020-04-28 20:32:42 +0800

    update

--- keylol.js
+++ keylol.js
@@ -36,14 +36,14 @@
     // 资源消耗大，谨慎使用
     // 通过更加详细的选择器来变相停止节点插入循环
 
-    var createElement = function (){
+    var createElement = function () {
 
-    let body = document.querySelector(`body`)
-    // nav-menu添加新parent
-     let navMenuParent = document.createElement(`nav`)
-     navMenuParent.id = `nav-menu-parent`
+        let body = document.querySelector(`body`)
+        // nav-menu添加新parent
+        let navMenuParent = document.createElement(`nav`)
+        navMenuParent.id = `nav-menu-parent`
 
-    document.querySelector(`body`).insertBefore(navMenuParent,document.querySelector('#nav-menu'))
+        document.querySelector(`body`).insertBefore(navMenuParent, document.querySelector('#nav-menu'))
 
     }
 
@@ -60,13 +60,13 @@
 
         // 用户栏&LOGO
         navMenu.insertBefore(tbContainer, null)
-        navMenu.insertBefore(document.querySelector('#nav-additional>#nav-user-action-bar'),null)
+        navMenu.insertBefore(document.querySelector('#nav-additional>#nav-user-action-bar'), null)
         navMenu.insertBefore(logo, navMenu.childNodes[0])
-        navMenuParent.insertBefore(navMenu,null)
+        navMenuParent.insertBefore(navMenu, null)
 
     }
 
-    var listenElement = function(){
+    var listenElement = function () {
         moveElement()
     }
 
@@ -77,77 +77,78 @@
     // DOM加载完全后------------------------
     // fetch热门主题图片
     let i = 1
-    function fetchHotImg(){
+    function fetchHotImg() {
 
         let imgRegx = /\"(https:\/\/blob\.keylol\.com\/forum.+?)\"/s
 
         let hotImgPostUrls = document.querySelectorAll(`.slideshow>li>a`)
 
-        hotImgPostUrls.forEach(postUrl=>{
+        hotImgPostUrls.forEach(postUrl => {
 
             //console.log(postUrl.href)
 
-         fetch(postUrl.href).then(function(res){
+            fetch(postUrl.href).then(function (res) {
                 return res.text();
-            }).then(bodyText=>{
+            }).then(bodyText => {
 
-         let imgUrls = bodyText.match(imgRegx)
-         if(imgUrls.length > 1){
+                let imgUrls = bodyText.match(imgRegx)
+                if (imgUrls.length > 1) {
 
-             let imgNode = postUrl.children[0]
-             postUrl.insertBefore(document.createElement(`div`),imgNode)
+                    let imgNode = postUrl.children[0]
+                    postUrl.insertBefore(document.createElement(`div`), imgNode)
 
-             // 去除默认尺寸
-             imgNode.removeAttribute("height")
-             imgNode.removeAttribute("width")
-             imgNode.src = bodyText.match(imgRegx)[0].replace(/"/g,``)
+                    // 去除默认尺寸
+                    imgNode.removeAttribute("height")
+                    imgNode.removeAttribute("width")
+                    imgNode.src = bodyText.match(imgRegx)[0].replace(/"/g, ``)
 
-             // 去除slidebar序号
-             document.querySelector(`.slidebar > ul >li:nth-child(${i})`).innerText = ``
-             i++
-         }
-         })
+                    // 去除slidebar序号
+                    document.querySelector(`.slidebar > ul >li:nth-child(${i})`).innerText = ``
+                    i++
+                }
+            })
         })
     }
 
     // 热门主题列表滚动监器
-    function hotPostShowMore(){
+    function hotPostShowMore() {
         let tabPContent = document.querySelector(`#tabPAhn0P_content`)
 
-        let tabShow = function(event){
-if(tabPContentShow.value === `hide`){
-tabPContent.classList.add(`tabPContentShow`)
-    event.target.value = `show`
+        let tabShow = function (event) {
+            if (tabPContentShow.value === `hide`) {
+                tabPContent.classList.add(`tabPContentShow`)
+                event.target.value = `show`
+                event.target.className = `tabPContentBtnShow`
 
-}else{
-tabPContent.classList.remove(`tabPContentShow`)
-    event.target.value = `hide`
-}
-
-
-
+            } else {
+                tabPContent.classList.remove(`tabPContentShow`)
+                event.target.value = `hide`
+                event.target.className = `tabPContentBtnHide`
+            }
         }
 
 
 
         let tabPContentShow = document.createElement(`button`)
         tabPContentShow.id = `tabPContentShow`
-        tabPContentShow.innerText=`...+5`
+        tabPContentShow.innerText = `...+5`
         tabPContentShow.value = `hide`
+        tabPContentShow.className = `tabPContentBtnHide`
+
 
         tabPContent.append(tabPContentShow)
-        tabPContentShow.addEventListener(`click`,tabShow)
+        tabPContentShow.addEventListener(`click`, tabShow)
 
     }
 
 
-     // symbol使用
+    // symbol使用
     let symbol = function (id) {
         return `<svg class="icon" aria-hidden="true"><use xlink:href="#${id}"></use></svg>`
     }
 
 
-    let windowLoad = function(){
+    let windowLoad = function () {
         console.log(`add symbol icons`)
         fetchHotImg()
         console.log(`add tabPAHn0P_content show more button`)
@@ -155,11 +156,11 @@ tabPContent.classList.remove(`tabPContentShow`)
     }
 
     // DOM加载后
-    document.addEventListener("DOMContentLoaded", function(event) {
-       windowLoad()
-       document.querySelector('#nav-logo').innerHTML += symbol(`kelolsteam_panel_hot`)
-       
-});
+    document.addEventListener("DOMContentLoaded", function (event) {
+        windowLoad()
+        document.querySelector('#nav-logo').innerHTML += symbol(`kelolsteam_panel_hot`)
+
+    });
 
 
     var css = `
