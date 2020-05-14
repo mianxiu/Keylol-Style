45f65c15c8dbf3bd8f8c7a186998b14ca90c624e
mianxiu <mianxiu@users.noreply.github.com>
2020-05-14 23:00:07 +0800

    fix name

--- card-for-keylol.user.js
+++ card-for-keylol.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         keylol css
+// @name         card for keylol
 // @namespace    http://tampermonkey.net/
-// @version      0.11.3
+// @version      0.11.4
 // @description  a style for keylol.com
 // @author       mianxiu
 // @match        keylol.com/*
